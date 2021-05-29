3f85ca5be5861ecc5593eef8b3408e53d2f99447
CodeLapseLogger <suryatej.nim@yahoo.com>
2021-05-29 16:14:56 +0530

    Implemented Static caching in EComm PWA

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,27 @@
 self.addEventListener('install', (event) => {
+
+    event.waitUntil(
+        caches.open('static-assets')
+        .then((cache) => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/offline/index.html',
+                'https://fonts.googleapis.com/icon?family=Material+Icons',
+                'https://code.getmdl.io/1.3.0/material.brown-orange.min.css',
+                '/src/css/home.css',
+                '/src/css/new_product.css',
+                'https://code.getmdl.io/1.3.0/material.min.js',
+                '/src/js/app.js',
+                '/src/js/product_listing.js',
+                '/src/js/new_product.js',
+                '/src/images/online-store-large-1100x513.png',
+                '/src/images/offline.jpg',
+                '/src/images/empty-store.jpg'
+            ])
+        })
+    );
+
     console.log('[Service Worker Log] Installing service worker...');
 });
 
@@ -10,5 +33,17 @@ self.addEventListener('activate', (event) => {
 
 self.addEventListener('fetch', (event) => {
     console.log(`[Service Worker Log] Requested resource: ${event.request.url}`);
-    event.respondWith(fetch(event.request));
+    event.respondWith(
+        caches.match(event.request)
+        .then((response) => {
+            if (response) {
+                return response;
+            } else {
+                return fetch(event.request)
+                    .catch((err) => {
+                        return caches.match('/offline/index.html');
+                    });
+            }
+        })
+    );
 });
